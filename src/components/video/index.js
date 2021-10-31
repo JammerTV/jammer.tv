25ea08502e923b4e0c03035b0b06d6fc09b3dedd
Mike Kasprzak <mikekasprzak@gmail.com>
2021-10-31 13:18:28 -0400

    in theory real dat

--- src/components/video/index.js
+++ src/components/video/index.js
@@ -3,7 +3,7 @@ import style from './style.css';
 
 const Video = ({channel}) => (
 	<div class={style.video}>
-        <iframe src={"https://player.twitch.tv/?channel="+channel+"&parent=new.jammer.tv"} frameborder="0" allowfullscreen="true" allow="autoplay; fullscreen" scrolling="no"></iframe>
+        <iframe src={"https://player.twitch.tv/?channel="+channel+"&parent=new.jammer.tv"} frameborder="0" allow="autoplay; fullscreen" scrolling="no"></iframe>
 	</div>
 );
 
--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -3,21 +3,63 @@ import style from './style.css';
 import Video from '../../components/video';
 import Item from '../../components/item';
 
-//const Home = () => {
+const StreamFeeds = [
+	"https://api.jammer.tv/v1/get/cat/ludum-dare",
+	"https://api.jammer.tv/v1/get/cat/demoscene",
+	"https://api.jammer.tv/v1/get/cat/game-jam",
+	"https://api.jammer.tv/v1/get/cat/game-dev"
+];
+
 class Home extends Component {
 	state = {
-		channel: "esl_sc2"
+		channel: "esl_sc2",
+		feed: []
+	}
+
+	componentDidMount() {
+		// https://api.jammer.tv/v1/get/cat/ludum-dare
+		// https://api.jammer.tv/v1/get/cat/game-dev
+
+		fetch('https://api.jammer.tv/v1/get/cat/game-jam')
+			.then(r => r.json())
+			.then(data => {
+				//console.log(data)
+				this.addFeed(data.streams);
+			});
 	}
 
 	setChannel = (value) => {
-		console.log("click: "+value.srcElement.alt, value);
+		console.log("setChannel: "+value.srcElement.alt, value);
 		this.setState({channel: value.srcElement.alt});
+	}
+
+	addFeed(feed) {
+		console.log("addFeed", feed);
+		if (!Array.isArray(feed) || (feed.length == 0)) {
+			return;
+		}
+
+		console.log("state before", this.state);
+		this.setState({'feed': this.state.feed.concat(feed)}, (feed) => {
+			console.log("state after", this.state);
+
+			// if no channel is set, use the first one
+			if (this.state.channel == "") {
+				this.setState({channel: feed[0].user_slug});
+			}
+		});
 	}
 
 	render(props, state) {
 		var items = [];
+		console.log("Render Feed", state.feed);
+		for (let idx = 0; idx < state.feed.length; ++idx) {
+			items.push(<Item channel={state.feed[idx].user_slug} onClick={this.setChannel} />);
+		}
+		/*
 		items.push(<Item channel="esl_sc2" onClick={this.setChannel} />);
 		items.push(<Item channel="monstercat" onClick={this.setChannel} />);
+		*/
 
 		return (
 			<div class={style.home}>
