cd7a114c918e09dfad2f5bf3f5d31c9cb2336e00
Mike Kasprzak <mikekasprzak@gmail.com>
2021-10-31 22:51:18 -0400

    better about

--- src/routes/about/index.js
+++ src/routes/about/index.js
@@ -8,8 +8,8 @@ const About = () => {
 			<div>
 				<h1>About Jammer.tv</h1>
 				<p>Jammer.tv is an aggregator of live video content. We primarily track Game Jams, Game Development, and Demoscene content, but in general we're interested in showcasing creativity with computers and code.</p>
-				<p>Jammer.tv began as LDTV, a spinoff of the <a href="https://ludumdare.com">Ludum Dare</a> game jam event, aggregating live video from popular streaming platforms like Twitch and YouTube.</p>
-				<p>The streams aggregated by Jammer.tv can be seen on Ludum Dare's event website: <a href="https://ldjam.com">ldjam.com</a></p>
+				<p>Jammer.tv began as LDTV, a spinoff of the <a href="https://ludumdare.com">Ludum Dare</a> game jam event, aggregating live video streams from participants on popular streaming platforms like Twitch and YouTube.</p>
+				<p>Today, streams aggregated by Jammer.tv can be seen on Ludum Dare's event website: <a href="https://ldjam.com">ldjam.com</a></p>
 			</div>
 		</div>
 	);
